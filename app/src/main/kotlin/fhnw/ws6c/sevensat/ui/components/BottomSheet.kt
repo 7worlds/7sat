db19fb83f93a639282ad3741ba0dd1442452770a
unknown <elias.braem@students.fhnw.ch>
2022-11-08 14:49:49 +0100

    add video and ISS content

--- app/src/main/kotlin/fhnw/ws6c/sevensat/ui/components/BottomSheet.kt
+++ app/src/main/kotlin/fhnw/ws6c/sevensat/ui/components/BottomSheet.kt
@@ -5,24 +5,31 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.FilterAlt
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
 import fhnw.ws6c.R
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun BottomSheet(scope: CoroutineScope, scaffoldState: BottomSheetScaffoldState) {
-  val image: Painter = painterResource(id = R.drawable.bob)
+  val image: Painter = painterResource(id = R.drawable.iss)
   Column (modifier = Modifier
     .padding(20.dp)
     .fillMaxWidth()) {
     Row (modifier = Modifier.fillMaxWidth(),
       horizontalArrangement = Arrangement.SpaceBetween,){
-      Text(text = "Name", style = MaterialTheme.typography.h1)
+      Text(text = "International Space Station (ISS)", style = MaterialTheme.typography.h1)
       IconButtonSat(onClick =  {
         scope.launch {
           if (scaffoldState.bottomSheetState.isCollapsed) {
@@ -44,12 +51,48 @@ fun BottomSheet(scope: CoroutineScope, scaffoldState: BottomSheetScaffoldState) 
     Row (modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween){
       Column {
         Text(text = "Position", style = MaterialTheme.typography.h2)
-        Text(text = "lat", style = MaterialTheme.typography.body1)
-        Text(text = "long", style = MaterialTheme.typography.body1)
+        Text(text = "lat: 23436456i34834593", style = MaterialTheme.typography.body1)
+        Text(text = "long: 83745983274598235", style = MaterialTheme.typography.body1)
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Text(text = "Höhe", style = MaterialTheme.typography.h2)
+        Text(text = "3000m", style = MaterialTheme.typography.body1)
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Text(text = "Sichtbarkeit", style = MaterialTheme.typography.h2)
+        Text(text = "Ja", style = MaterialTheme.typography.body1)
+
 
       }
-      Image(painter = image, contentDescription = "Bild")
+      Image(painter = image, contentDescription = "Bild", modifier = Modifier.width(150.dp))
     }
-    Text(text = "Beschreibung", style = MaterialTheme.typography.body1)
+    Spacer(modifier = Modifier.height(20.dp))
+    Text(text = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.", style = MaterialTheme.typography.body1)
+    Spacer(modifier = Modifier.height(20.dp))
+    YoutubePlayer(key = "86YLFOog4GM")
+    
   }
+}
+@Composable
+fun YoutubePlayer(key: String) {
+  val activityLifecycle = LocalLifecycleOwner.current.lifecycle
+  val context = LocalContext.current
+  val youtubePlayer = remember(key) {
+    YouTubePlayerView(context).apply {
+      activityLifecycle.addObserver(this)
+      enableAutomaticInitialization = false
+      initialize(object : AbstractYouTubePlayerListener() {
+        override fun onReady(youTubePlayer: YouTubePlayer) {
+          youTubePlayer.cueVideo(key, 0f)
+        }
+      })
+    }
+  }
+  AndroidView(
+    factory = { youtubePlayer },
+    modifier = Modifier
+      .fillMaxWidth()
+      .wrapContentHeight(),
+    update = {}
+  )
 }
