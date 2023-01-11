496b9e797c7633c4a04251166737ac5bbc5584dc
Andri Wild <andri.wild@students.fhnw.ch>
2023-01-11 11:06:11 +0100

    update celestrak calls to be generic

--- app/src/main/kotlin/fhnw/ws6c/sevensat/data/celestrak/TleAllCall.kt
+++ app/src/main/kotlin/fhnw/ws6c/sevensat/data/celestrak/TleAllCall.kt
@@ -1,33 +1,7 @@
 package fhnw.ws6c.sevensat.data.celestrak
 
-import fhnw.ws6c.sevensat.data.ApiCallable
-
 const val celestrakBaseURL = "https://celestrak.org/NORAD/elements/gp.php"
 
-
-class TleAllCall: ApiCallable<Map<Long, Triple<String, String, String>>> {
-  var mapResponse: Map<Long, Triple<String, String, String>>?   = null
-  var exception:    Exception?    = null
-
+class TleAllCall: CelesTrakCall<Map<Long, Triple<String, String, String>>>() {
   override fun getTargetUrl(): String = "$celestrakBaseURL?GROUP=active&FORMAT=tle"
-
-  override fun getResponse(): Map<Long, Triple<String, String, String>>? {
-    return mapResponse
-  }
-
-  override fun setResponse(response: Map<Long, Triple<String, String, String>>) {
-    mapResponse = response
-  }
-
-  override fun hasError(): Boolean {
-    return exception != null
-  }
-
-  override fun getError(): Exception? {
-    return exception
-  }
-
-  override fun setError(e: Exception) {
-    exception = e
-  }
 }
